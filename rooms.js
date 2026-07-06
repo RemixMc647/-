1dd8b51e8fd67f970cc96b4e84b85980079bfe9d
Adelabu Aderemi Rasheed <adelabuaderemi02@gmail.com>
2026-07-06 01:49:56 +0100

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -7,11 +7,11 @@ drift out of sync with each other.
 const DEFAULT_ROOMS = [
   { id: 'lounge', name: 'Just Chatting Lounge' },
   { id: 'minecraft', name: '🧱 Minecraft' },
-  { id: 'roblox', name: '🎯 Roblox' },
+  { id: 'roblox', name: '🔲 Roblox' },
   { id: 'fortnite', name: '🪂 Fortnite Squads' },
   { id: 'cod', name: '🔫 Call of Duty' },
   { id: 'freefire', name: '🔥 Free Fire' },
-  { id: 'pubg', name: '🪖 PUBG' },
+  { id: 'pubg', name: '🥽 PUB-G' },
   { id: 'gta', name: '🚗 GTA' },
   { id: 'amongus', name: '🚀 Among Us' },
 ];
